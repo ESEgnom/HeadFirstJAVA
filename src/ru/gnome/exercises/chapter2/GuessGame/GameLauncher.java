83ec5b546d0981ee2ad9b20c03ae6e103d85e7b9
gnomese <gnomese89@gmail.com>
2021-09-27 17:09:58 +0500

    Вторая глава

---
+++ src/ru/gnome/exercises/chapter2/GuessGame/GameLauncher.java
@@ -0,0 +1,8 @@
+package ru.gnome.exercises.chapter2.GuessGame;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ src/ru/gnome/exercises/chapter2/TapeDeck/TapeDeck.java
@@ -0,0 +1,12 @@
+package ru.gnome.exercises.chapter2.TapeDeck;
+
+public class TapeDeck {
+    boolean canRecord;
+    void playTape(){
+        System.out.println("пленка проигрывается");
+    }
+    void recordTape(){
+        System.out.println("идет запись на пленку");
+    }
+
+}
